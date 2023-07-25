2ef2804b967a647b05ad870d20fe424bbaa93d26
Craig Askings <craig@askings.com.au>
2023-07-25 12:14:55 +1000

    Add extra context to the Secret Key ImproperlyConfigured exception
    Added extra information in the exception on where to update the secret key settings.

--- netbox/netbox/settings.py
+++ netbox/netbox/settings.py
@@ -74,7 +74,9 @@ if type(SECRET_KEY) is not str:
 if len(SECRET_KEY) < 50:
     raise ImproperlyConfigured(
         f"SECRET_KEY must be at least 50 characters in length. To generate a suitable key, run the following command:\n"
-        f"  python {BASE_DIR}/generate_secret_key.py"
+        f"  python {BASE_DIR}/generate_secret_key.py\n"
+        f"Use the output to update the [SECRET_KEY] section of the configuration file $INSTALL_ROOT/netbox/netbox/configuration.py\n"
+        f"https://demo.netbox.dev/static/docs/configuration/required-parameters/#secret_key"
     )
 
 # Calculate a unique deployment ID from the secret key
